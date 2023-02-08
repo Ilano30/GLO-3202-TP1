9644ce8efa7a570e53d64bc247439aef7911ad9a
Ilan <ilan.benarrochepro@gmail.com>
2023-02-08 16:22:38 -0500

    [UPD] Change server.js file

--- server.js
+++ server.js
@@ -1,66 +1,11 @@
-const cors = require("cors");
-const { validate } = require("jsonschema");
-const express = require("express"),
-  serveStatic = require("serve-static"),
-  history = require("connect-history-api-fallback"),
-  fs = require("fs"),
-  port = process.env.PORT || 8000;
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
-const app = express();
-app.use(express.json());
+const app = express()
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-const listSchema = {
-  type: "array",
-  default: [],
-  items: {
-    type: "object",
-    required: ["name", "id"],
-    properties: {
-      name: {
-        type: "string",
-      },
-      id: {
-        type: "integer",
-      },
-    },
-  },
-};
-
-app.get("/task", (req, res) => {
-  filePath = __dirname + "/data.txt";
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    if (!data) res.send("[]");
-    else res.send(data);
-  });
-});
-
-app.post("/task", (req, res) => {
-  const result = validate(req.body, listSchema);
-  if (!result.valid) {
-    res.sendStatus(400);
-    return;
-  }
-  const data = JSON.stringify(req.body);
-  filePath = __dirname + "/data.txt";
-  fs.writeFile(filePath, data, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-  res.sendStatus(200);
-});
-
-app.use(serveStatic(__dirname + "/dist/spa"));
-app.use(history());
-app.listen(port);
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa'))
+app.listen(port)
